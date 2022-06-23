fc729fdb9d0aba466a72a344b46019577df947ce
Twisha Chachra <twishachachra@gmail.com>
2022-06-23 08:45:15 -0400

    Initial commit

---
+++ src/main/java/com/example/midterm200474646/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.midterm200474646;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.midterm200474646 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.midterm200474646 to javafx.fxml;
+    exports com.example.midterm200474646;
+}
